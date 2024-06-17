a255b941c576630e4cb8ef8f21cb618c14ad55b3
万顷茫然 <121007356+zxunge@users.noreply.github.com>
2024-06-17 10:17:58 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -23,11 +23,11 @@ cd fltk-${FLTK_VERSION}
 ./configure --prefix=/home/FLTK ${BUILD_INFO}
 mingw32-make
 
-7zr a -mx9 -mqs=on -mmt=on /home/${NAME}.7z /home/fltk-${FLTK_VERSION}
+7zr a -mx9 -mqs=on -mmt=on /home/${NAME}-${BUILD_NO}.7z /home/fltk-${FLTK_VERSION}
 
 if [[ -v GITHUB_WORKFLOW ]]; then
   echo "OUTPUT_BINARY=${HOME}/${NAME}.7z" >> $GITHUB_OUTPUT
-  echo "RELEASE_NAME=fltk-${FLTK_VERSION}" >> $GITHUB_OUTPUT
+  echo "RELEASE_NAME=fltk-${FLTK_VERSION}-${BUILD_NO}" >> $GITHUB_OUTPUT
   echo "FLTK_VERSION=${FLTK_VERSION}" >> $GITHUB_OUTPUT
   echo "OUTPUT_NAME=${NAME}.7z" >> $GITHUB_OUTPUT
   echo "BUILD_INFO=${BUILD_INFO}" >> $GITHUB_OUTPUT
